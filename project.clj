3c0ac8808a1d1f8705f4771b718dd8e7b263871e
Hilaire Bouaddi <hilaire.bouaddi@gmail.com>
2021-04-17 17:00:24 +0200

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject project1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.cognitect.aws/api "0.8.505"]
+                 [com.cognitect.aws/endpoints "1.1.11.976"]
+                 [com.cognitect.aws/s3 "811.2.865.0"]]
+  :repl-options {:init-ns project1.core})
---
+++ src/project1/core.clj
@@ -0,0 +1,4 @@
+(ns project1.core)
+
+(require '[cognitect.aws.client.api :as aws])
+
